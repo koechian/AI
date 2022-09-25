a7362afbf9a78c321e7ee1a66e9227da54e0a85a
koechian <ianovic8@live.com>
2022-09-25 23:05:08 +0300

    Uniform Cost Search done

--- Lab 2/C-135357.py
+++ Lab 2/C-135357.py
@@ -1,25 +1,74 @@
 # imports
 
+from queue import PriorityQueue
 import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def UCS(graph, start, goal):
-    # opened, closed = [], []
+def ucs(graph, start, goal):
+    # used the end='' to prevent skipping a line
+    print("Uniform Cost Search:", end="")
 
-    print("Uniform Cost Search:")
-    # return path
+    # a queue is used to store the nodes and a list to keep track of visited nodes
+    q = PriorityQueue()
+    # start node is marked as visited
+    q.put((0, start))
+
+    visited = []
+
+    while True:
+        if q.empty():
+            print("Goal not attainable")
+            return False
+
+        w, current = q.get()
+        # print(current)
+        visited.append(current)
+
+        if current == goal:
+            print(visited)
+            return
+
+        for node in graph[current]:
+            q.put((w + graph_costs(graph, current, node), node))
 
 
-def DDFS(range):
-    print("Iterative Deepening DFS:")
-    # return path
-    pass
+def graph_costs(graph, start, end):
+    return graph[start][end]["weight"]
+
+
+def ddfs(graph, start, goal, depth):
+    # recursively calles the helper(DLS) until it reaches exhausts all nodes in the range
+    print("Iterative Deepening DFS:" + start + "->", end="")
+    # used the end='' to print on one line
+    for i in range(depth):
+        if ddfs_helper(graph, start, goal, i):
+            return True
+    return False
+
+
+def ddfs_helper(graph, start, goal, depth):
+
+    # checks if the start node is the goal node
+    if start == goal:
+        return True
+
+    # checks if the range has been attained
+    if depth <= 0:
+        return False
+
+    # recurse for all next nodes
+    for i in graph[start]:
+        # this is the list to be output
+        if ddfs_helper(graph, i, goal, depth - 1):
+            ddfs_list.append(i)
+            return True
+
+    return False
 
 
 def draw(graph):
     # drawing the graph
-
     # randomizing node position on the graph
     pos = nx.spring_layout(graph, k=1)
 
@@ -105,4 +154,17 @@ for x, y in graph.items():
         # where x is the current node, a is the neighbour and b is the cost of the edge
         DG.add_edge(x, a, weight=b)
 
-draw(DG)
+# Main Code
+# list to store ddfs results
+ddfs_list = []
+
+
+# function used to visualize the graph uncomment to see the graph weights and links
+# draw(DG)
+
+# Iterative DDFS
+ddfs(DG, "A", "N", 10)
+# reversing the list using a negative step
+print(ddfs_list[::-1])
+
+ucs(DG, "A", "N")
